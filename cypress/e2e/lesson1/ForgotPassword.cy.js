9e6f8b9585b24d25d80c59aa343602a04a4066d5
#Grobo813663 <laurabolyan1192@gmail.com>
2025-01-13 16:00:14 -0800

    Forgot Pass Refactoring

---
+++ cypress/e2e/lesson1/ForgotPassword.cy.js
@@ -0,0 +1,49 @@
+const { forgotPasswordPage } = require("../../pages/ForgotPassword");
+const { generateRandomName } = require("../../utils/function");
+const { LOCATORS } = require("../../utils/locators");
+
+const randomEmail = `${generateRandomName(6)}"@gmail.com"`;
+let value;
+const label = "label";
+const email = "E-mail";
+
+describe("Forgot email form", () => {
+  it("Verify 'Email' page", () => {
+    cy.visit("/");
+    cy.contains(forgotPasswordPage.NAMES.forgotPasswordPageTitle).click();
+    cy.get(LOCATORS.example).should(
+      "contain",
+      forgotPasswordPage.NAMES.forgotPasswordPageTitle
+    );
+    cy.url().should("include", forgotPasswordPage.NAMES.linkName);
+    cy.get(LOCATORS.example).should(
+      "contain",
+      forgotPasswordPage.NAMES.forgotPasswordPageTitle
+    );
+    cy.get(forgotPasswordPage.LOCATORS.row)
+      .find(label)
+      .should("contain", email);
+    cy.get(forgotPasswordPage.LOCATORS.email).should("be.empty");
+    cy.get(forgotPasswordPage.LOCATORS.form).should(
+      "have.css",
+      "background-color",
+      forgotPasswordPage.COLORS.blue
+    );
+  });
+})
+
+  it("Verify unique emails input", () => {
+    cy.visit("/");
+    cy.contains(forgotPasswordPage.NAMES.forgotPasswordPageTitle).click();
+    cy.get(forgotPasswordPage.LOCATORS.email).type(randomEmail);
+    cy.get(forgotPasswordPage.LOCATORS.email)
+      .invoke("val")
+      .then((val) => {
+        value = val;
+      });
+      cy.then(()=>{
+        expect(value).to.eq(randomEmail)
+      })
+      cy.get(forgotPasswordPage.LOCATORS.form).click()
+      cy.get('h1').should("contain", "Internal Server Error")
+});
